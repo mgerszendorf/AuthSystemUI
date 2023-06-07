c4d7bfbb0de6b84784d922f4039890d7f667598e
marek_gerszendorf <marek.gerszendorf@wp.pl>
2023-06-07 18:44:02 +0200

    Initial Commit

---
+++ AuthSystemUI/AuthSystemUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthSystemUIApp.swift
+//  AuthSystemUI
+//
+//  Created by Marek Gerszendorf on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AuthSystemUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
